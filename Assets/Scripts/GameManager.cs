81cc90d8d113132ea2b2bb874644b088b5c75bbf
bbgoly <bbgoly@users.noreply.github.com>
2020-01-22 10:19:25 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,9 +21,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        int width = Random.Range(1, 4), height = Random.Range(1, 4);
-        FillArea(width, height);
-        GenerateSides(width, height);
+        GenerateDungeon(Random.Range(1, 4), Random.Range(1, 4));
     }
 
     private void Update()
@@ -31,27 +29,20 @@ public class GameManager : MonoBehaviour
 
     }
 
-    private void GenerateSides(int width, int height)
-    {
-        foreach (Vector2 floorPos in floorGrid)
-        {
-            GameObject wallTile;
-            if (floorPos.x == 0)
-            {
-                wallTile = Instantiate(verticalWall, floorPos, Quaternion.identity);
-            }
-        }
-    }
-
-    private void FillArea(int width, int height)
+    private void GenerateDungeon(int width, int height)
     {
         for (float x = 0; x <= width; x += 0.15f)
         {
             for (float y = 0; y <= height; y += 0.15f)
             {
-                Vector2 tilePostion = new Vector2(x - 1, y);
-                GameObject floorTile = Instantiate(floorTiles[Random.Range(0, floorTiles.Count)], tilePostion, Quaternion.identity);
-                floorGrid.Add(tilePostion);
+                Vector2 tilePosition = new Vector2(x - 1, y);
+                GameObject floorTile = Instantiate(floorTiles[Random.Range(0, floorTiles.Count)], tilePosition, Quaternion.identity);
+				floorGrid.Add(tilePosition);
+				if (x >= width || y >= height)
+				{
+					GameObject wallTile = Instantiate(wallPrefab, tilePosition, Quaternion.Eular(0, 0, x >= width ? 90 : 0));
+					wallTile.Add(tilePosition);
+				}
             }
         }
     }
